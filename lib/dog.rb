c6c74f5931338a5f1dce6b7b3d74fafe128ab074
Megan Christine McClure <mcclme02@luther.edu>
2020-03-17 14:39:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,21 +16,3 @@
   
 # end
 
-class Dog
-  attr_accessor :name   #has a name
-  
-  @@all = [ ]   
-
-  def initialize(name)    #.new initializes an argument of a name
-    @name = name
-    @@all << self     #adds new dog to the @@all array. New dog instances/objects are called self
-  end
-
-  def self.all
-    puts @@all.collect { |dog| dog.name } #class method that puts out the name of each dog
-  end
-
-  def self.clear_all   #class method that empties the @@all array of all existing dogs
-    @@all.clear
-  end
-end
